137e30643821687f31813144c996cd7317e665e8
scehovic <scehovic@iastate.edu>
2022-11-08 21:19:45 +0000

    Upload New File

---
+++ tests/RyanScehovic/Main.h
@@ -0,0 +1,20 @@
+/*
+ * Main.h
+ *
+ *      Author: ryan scehovic
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <openssl/bio.h> /* BasicInput/Output streams */
+#include <openssl/err.h> /* errors */
+#include <openssl/ssl.h> /* core library */
+#include <openssl/evp.h>
+
+#endif /* MAIN_H_ */
