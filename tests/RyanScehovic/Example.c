5a8f922710c1bd858b1a25fb3cf6a3066690fa30
scehovic <scehovic@co1313-08.ece.iastate.edu>
2022-11-10 12:32:37 -0600

    more work on trying to encrypt/decrypt CAN frame

--- tests/RyanScehovic/Example.c
+++ tests/RyanScehovic/Example.c
@@ -1,3 +1,5 @@
+#include "Main.h"
+
 static const unsigned char key[] = {
     0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
     0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff,
@@ -5,8 +7,48 @@ static const unsigned char key[] = {
     0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f
 };
 
-int main()
-{
+int main(int argc, char *argv[]){
+    FILE *testDataFile;
+     testDataFile = fopen("../../CAN_log_files/Sept 13th Files/Full_CAN_Bus_Log.asc", "r");
+
+     if(testDataFile == NULL){
+       printf("couldn't make it to file :(");
+       return;
+     }
+
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    unsigned char lineFromFile[68];
+    unsigned char canData[24];
+    read = getline(&line, &len, testDataFile);
+    strcpy(lineFromFile, line);
+
+    // printf("\n line from file = %s", lineFromFile);
+    int a;
+    int b=0;
+
+    //identifies start off message to encrypt
+    for(a=0; a < 70; a++){
+      if(lineFromFile[a] == 'd' && lineFromFile[a+2] == '8'){
+	a+= 4;
+	//printf("\nstarting char: %c %c\n", lineFromFile[a], lineFromFile[67]);
+	break;
+      }
+    }
+    int z;
+    for(z=a; lineFromFile[z]!='\0'; z++){
+      canData[b]=lineFromFile[z];
+      b++;
+      //printf("%c\n", lineFromFile[z]);
+    }    
+    int x;
+    //    for(x=0; x<24; x++)
+    // {
+	//	printf("%c", canData[x]);
+	//     }
+	// printf("\n");
+    
     unsigned char text[]="hello world!";
     unsigned char enc_out[80];
     unsigned char dec_out[80];
@@ -14,23 +56,31 @@ int main()
     AES_KEY enc_key, dec_key;
 
     AES_set_encrypt_key(key, 128, &enc_key);
-    AES_encrypt(text, enc_out, &enc_key);      
+    AES_encrypt(canData, enc_out, &enc_key);      
 
     AES_set_decrypt_key(key,128,&dec_key);
     AES_decrypt(enc_out, dec_out, &dec_key);
 
-    int i;
+    //  int i;
 
-    printf("original:\t");
-    for(i=0;*(text+i)!=0x00;i++)
-        printf("%X ",*(text+i));
-    printf("\nencrypted:\t");
-    for(i=0;*(enc_out+i)!=0x00;i++)
-        printf("%X ",*(enc_out+i));
-    printf("\ndecrypted:\t");
-    for(i=0;*(dec_out+i)!=0x00;i++)
-        printf("%X ",*(dec_out+i));
-    printf("\n");
+
+    printf("\ncanData string: %s\n", canData);
+    printf("\nencrypted:\t %s", enc_out);
+    printf("\n\ndecrypted:\t %s\n", dec_out);
+
+    
+
+
+    //printf("\noriginal:\t");
+    //for(i=0;*(canData+i)!=0x00;i++)
+    // printf("%X ",*(canData+i));
+    // printf("\n\nencrypted:\t");
+    //for(i=0;*(enc_out+i)!=0x00;i++)
+    // printf("%X ",*(enc_out+i));
+    // printf("\n\ndecrypted:\t");
+    //for(i=0;*(dec_out+i)!=0x00;i++)
+    // printf("%X ",*(dec_out+i));
+    //printf("\n");
 
     return 0;
 } 
