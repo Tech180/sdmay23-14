b4c422249bcbbfcd67ebee59252720ecbaf34de7
scehovic <scehovic@vlinux-27.ece.iastate.edu>
2022-11-20 17:57:43 -0600

    starting ECU and Bridge files

--- tests/Phase2/ECU.c
+++ tests/Phase2/ECU.c
@@ -1,20 +1,52 @@
 #include "Main.h"
 
-void main(int argc, char *argv[]) {   
+void main(int argc, char *argv[]) {
+    char *ECU_type;
+    int a;
+    if(argc > 0){
+        ECU_type = argv[1]; //arguement passed in - something like 00x
+        printf("parameter passed in: ");
+	for(a=0; a < strlen(ECU_type); a++){
+	  printf("%c", ECU_type[a]);
+	}
+	printf("\n");
+    }
+    //example frame: 4419.967450 1     18FEDF00x        Rx    d 8 89 AE 41 FF FF FF FF 05
+    // the 00x represents engine
+  
     FILE *testDataFile;
     testDataFile = fopen("../../CAN_log_files/Sept 13th Files/Full_CAN_Bus_Log.asc", "r");
-
     if(testDataFile == NULL){
         printf("couldn't make it to file :(");
         return;
     }
      
     int i;
-    while((read = getline(&line, &len, testDataFile)) != -1 && cycle<3){
-        printf("original:\t");      
-        for(i=0;(read+i)!=0x00;i++)       
-	        printf("%X ",(read+i)); 
-     }   
-     fclose(testDataFile);
+    char * line = NULL;
+    unsigned char lineFromFile[70];
+    size_t len = 0;
+    ssize_t read;
+    char *lineType;
+    char type[4];
+    int counter=0;
+    int z;
+
+    //prints out all the lines where the main parameter (ex: 00x) is the same type as the line in the file
+    while((read = getline(&line, &len, testDataFile)) != -1){
+      counter++;
+      strcpy(lineFromFile, line);
+
+      memcpy(type, &line[24], 3);
+      type[3]= '\0';
+      lineType = &type[0]; //set pointer to start of type array (so that we can use strcmp()
+
+       //checking that types are equal -
+      //if the parameter to main is '00x' and the line of the file has '00x' they are both engine 
+      if(strcmp(ECU_type, lineType)==0){ 
+	printf("\npassed if statement \n ");
+	printf("line %d: %c%c%c  \n", counter, lineFromFile[24], lineFromFile[25], lineFromFile[26]);
+      }
+    }   
+    fclose(testDataFile);
      
 }
