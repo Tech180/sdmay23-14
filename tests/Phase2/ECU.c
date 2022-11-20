787250d690c1e60cb2eb0b947b9abc66d049da37
scehovic <scehovic@iastate.edu>
2022-11-20 21:38:14 +0000

    Add new file

---
+++ tests/Phase2/ECU.c
@@ -0,0 +1,21 @@
+#include "Main.h"
+
+void main(int argc, char *argv[]) {   
+    FILE *testDataFile;
+    testDataFile = fopen("../../CAN_log_files/Sept 13th Files/Full_CAN_Bus_Log.asc", "r");
+
+    if(testDataFile == NULL){
+        printf("couldn't make it to file :(");
+        return;
+    }
+
+     
+    while((read = getline(&line, &len, testDataFile)) != -1 && cycle<3){
+        printf("original:\t");      
+        for(i=0;(read+i)!=0x00;i++)       
+	        printf("%X ",(read+i)); 
+
+     }   
+     fclose(testDataFile);
+     
+}
