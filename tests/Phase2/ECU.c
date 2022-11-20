d6acda7c3ec7585ccf937dfbb4415e1421db21e3
scehovic <scehovic@iastate.edu>
2022-11-20 21:41:28 +0000

    Update tests/Phase2/ECU.c

--- tests/Phase2/ECU.c
+++ tests/Phase2/ECU.c
@@ -8,13 +8,12 @@ void main(int argc, char *argv[]) {
         printf("couldn't make it to file :(");
         return;
     }
-
      
+    int i;
     while((read = getline(&line, &len, testDataFile)) != -1 && cycle<3){
         printf("original:\t");      
         for(i=0;(read+i)!=0x00;i++)       
 	        printf("%X ",(read+i)); 
-
      }   
      fclose(testDataFile);
      
