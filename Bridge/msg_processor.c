4eac287f2a8b90cc91ca3dc46730bddd23827ede
Josue Torres <jatorres@iastate.edu>
2023-01-24 16:02:52 -0600

    added structs for queue and linked list structure

--- Bridge/msg_processor.c
+++ Bridge/msg_processor.c
@@ -3,4 +3,14 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+struct queue {
+	struct message * head, * tail;
+	int num_messages; 
+} queue; 
 
+struct threadWork { 
+	struct threadWork * next; 
+	struct threadWork * prev;
+	struct message * msg; 
+	int msg_id; 
+} threadWork; 
