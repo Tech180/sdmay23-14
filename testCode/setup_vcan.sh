f205c6245cdf646c212d3e455e20aa8c8e1a469a
Ryan <rpc1@kali.231.com>
2023-02-14 08:55:59 -0600

    uploaded john's code to the repo so we don't have to find it in outlook when in vm

---
+++ testCode/setup_vcan.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+sudo modprobe can
+sudo modprobe vcan
+
+sudo ip link add dev vcan0 type vcan
+sudo ip link set up vcan0
+ifconfig vcan0
+
+sudo ip link add dev vcan1 type vcan
+sudo ip link set up vcan1
+ifconfig vcan1
+
+
