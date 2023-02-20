90d785aa08f663f624283b0a6864bcd455519536
rpc1 <rpc1@iastate.edu>
2023-02-20 04:51:58 +0000

    update bitfield to 33 bits for all the data to send

--- Socket_CAN_Tests/sendFrame.py
+++ Socket_CAN_Tests/sendFrame.py
@@ -7,7 +7,7 @@ f = open("00x.txt")
 for x in f:
   
   can_id = int(x[24:26] + "" + x[27:29],16)
-  data = bytes.fromhex(x[0:24])
+  data = bytes.fromhex(x[0:33])
   
   frame1 = CanFrame(can_id=can_id, data=data)
 
