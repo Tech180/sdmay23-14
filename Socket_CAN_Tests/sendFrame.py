d6880889899ea755f193fecbf1dd51920f0c4831
Ryan Campbell <rpc1@iastate.edu>
2023-02-20 00:53:32 -0600

    scehovic and campbell got pgn and sa added into the sent can data

--- Socket_CAN_Tests/sendFrame.py
+++ Socket_CAN_Tests/sendFrame.py
@@ -3,11 +3,11 @@ from socketcan import *
 interface = "vcan0"
 s = CanRawSocket(interface=interface)
 
-f = open("00x.txt")
+f = open("00x2.txt")
 for x in f:
   
-  can_id = int(x[24:26] + "" + x[27:29],16)
-  data = bytes.fromhex(x[0:33])
+  can_id = int(x[16:18] + "" + x[18:20] + x[20:22],16)
+  data = bytes.fromhex(x[0:16])
   
   frame1 = CanFrame(can_id=can_id, data=data)
 
