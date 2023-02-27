925e21fba192086cd74dfddfa904e2fa24c5484a
scehovic <scehovic@iastate.edu>
2023-02-27 14:31:50 +0000

    Update Socket_CAN_Tests/sendFrame2.py

--- Socket_CAN_Tests/sendFrame2.py
+++ Socket_CAN_Tests/sendFrame2.py
@@ -1,18 +1,17 @@
 
-from socketcan import *
+import time
 import can
 
-interface = "vcan0"
-#s = CanRawSocket(interface=interface, fd=True)
-s = can.interfaces.socketcan.SocketcanBus(channel="vcan0", fd=True)
+bustype = 'socketcan'
+channel = 'vcan0'
 
+def producer(id):
+    """:param id: Spam the bus with messages including the data id."""
+    bus = can.Bus(channel=channel, interface=bustype)
+    for i in range(10):
+        msg = can.Message(arbitration_id=0xc0ffee, data=[id, i, 0, 1, 3, 1, 4, 1], is_extended_id=False)
+        bus.send(msg)
 
-f = open("00x2.txt")
-for x in f:
-  
-  can_id = int(x[16:18] + "" + x[18:20] + x[20:22],16)
-  data = bytes.fromhex(x[0:16]) + bytes.fromhex(x[0:16]) + bytes.fromhex(x[0:16]) + bytes.fromhex(x[0:16]) + bytes.fromhex(x[0:16])
-  
-  frame1 = CanFrame(can_id=can_id, data=data)
+    time.sleep(1)
 
-  s.send(frame1)
+producer(10)
