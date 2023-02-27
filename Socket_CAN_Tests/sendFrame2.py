e7ccf5c3d76d3231b38534e483986ebcb422d9c0
scehovic <scehovic@iastate.edu>
2023-02-27 15:40:40 +0000

    Update Socket_CAN_Tests/sendFrame2.py

--- Socket_CAN_Tests/sendFrame2.py
+++ Socket_CAN_Tests/sendFrame2.py
@@ -5,19 +5,25 @@ import can
 bustype = 'socketcan'
 channel = 'vcan0'
 
-
-def producer(id):
-    """:param id: Spam the bus with messages including the data id."""
-    #bus = can.Bus(channel=channel, interface=bustype)
-    bus = can.interfaces.socketcan.SocketcanBus(channel='vcan0', fd=True)
+def run():
+    bus = can.interfaces.socketcan.SocketcanBus(channel=channel, fd=True)
     f = open("00x2.txt")
+    lineCount = 1;
+    data_msg=[]
 
+#reads 5 lines from the file, adding each to data_msg[], then creates msg to send on bus
     for x in f:
-        can_id = int(x[16:18] + "" + x[18:20] + x[20:22],16)
-        data_msg = [bytes.fromhex(x[0:16])]
-        msg = can.Message(arbitration_id=can_id, data=data_msg, is_extended_id=False)
-        bus.send(msg)
+        if lineCount % 5 == 0:
+            can_id = int(x[16:18] + "" + x[18:20] + x[20:22],16) #PGN & SA
+            data_msg.append(can_id)
+            data_msg.append(bytes.fromhex(x[0:16]))
+            msg = can.Message(arbitration_id=can_id, data=data_msg, is_extended_id=False)
+            bus.send(msg)
+            data_msg=[]
+        else:
+            can_id = int(x[16:18] + "" + x[18:20] + x[20:22],16) #PGN & SA
+            data_msg.append(can_id)
+            data_msg.append(bytes.fromhex(x[0:16]))
+        lineCount++
 
-    time.sleep(1)
-
-producer(10)
+run()
