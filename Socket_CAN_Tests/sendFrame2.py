16895154f6b59eec63ee96987f586fdd7714eb0a
scehovic <scehovic@iastate.edu>
2023-02-27 13:35:39 +0000

    Update Socket_CAN_Tests/sendFrame2.py

---
+++ Socket_CAN_Tests/sendFrame2.py
@@ -0,0 +1,15 @@
+
+from socketcan import *
+
+interface = "vcan0"
+s = CanRawSocket(interface=interface, fd=True)
+
+f = open("00x2.txt")
+for x in f:
+  
+  can_id = int(x[16:18] + "" + x[18:20] + x[20:22],16)
+  data = bytes.fromhex(x[0:16]) + bytes.fromhex(x[0:16]) + bytes.fromhex(x[0:16]) + bytes.fromhex(x[0:16]) + bytes.fromhex(x[0:16])
+  
+  frame1 = CanFrame(can_id=can_id, data=data)
+
+  s.send(frame1)
