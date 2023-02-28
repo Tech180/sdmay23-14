0d787280dc6212026bc96d835288f24a9afc824e
scehovic <scehovic@iastate.edu>
2023-02-28 01:17:50 +0000

    Update Socket_CAN_Tests/sendFrame2.py

--- Socket_CAN_Tests/sendFrame2.py
+++ Socket_CAN_Tests/sendFrame2.py
@@ -14,7 +14,6 @@ def run():
 
 #reads 5 lines from the file, adding each to data_msg[], then creates msg to send on bus
     for x in f:
-        print("start of cycle ", lineCount)
         pgn_1 = int(x[16:18], 16)
         pgn_2 = int(x[18:20], 16)
         source_address = int(x[20:22],16)
@@ -22,18 +21,18 @@ def run():
         data_msg.append(pgn_1)
         data_msg.append(pgn_2)
         data_msg.append(source_address)
-        if lineCount % 5 == 0:
-            print("flag 1")
+
         for i in range(0,15,2):
             data_msg.append(int(x[i:i+1], 16))
+
         if lineCount % 5 == 0:
-            print("flag 2")
-        if lineCount % 5 == 0:
-            msg = can.Message(arbitration_id=0xabc123, data=data_msg, is_extended_id=False)
+            msg = can.Message(arbitration_id=0xabc123, data=data_msg, is_extended_id=True)
+            print("flag 1")
             bus.send(msg)
+            print("flag 2")
             data_msg=[]
             
         lineCount+=1
-        print(data_msg)
+        #print(data_msg)
 
 run()
