fe8dc87ae4b0bf74f84efc4b95abfc38b1f78d98
scehovic <scehovic@iastate.edu>
2023-02-28 00:27:32 +0000

    Update Socket_CAN_Tests/sendFrame2.py

--- Socket_CAN_Tests/sendFrame2.py
+++ Socket_CAN_Tests/sendFrame2.py
@@ -21,12 +21,12 @@ def run():
             msg = can.Message(arbitration_id=can_id, data=data_msg, is_extended_id=False)
             bus.send(msg)
             data_msg=[]
-            print(data)
+            print(data_msg)
         else:
             can_id = int(x[16:18] + "" + x[18:20] + x[20:22],16) #PGN & SA
             data_msg.append(can_id)
             data_msg.append(int(x[0:16], 16))
-            print(data)
+            print(data_msg)
         lineCount+=1
 
 run()
