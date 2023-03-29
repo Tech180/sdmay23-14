4e52cec1025c53d87eb41af751c163a11b0388f2
rjlawson <rjlawson@iastate.edu>
2023-03-29 01:15:01 -0500

    made a bridge function, made speed backwards compatible and more efficient with can and canfd, along with updated some calling in main to functions that weren't depricated... Testing needs to be done

--- Socket_CAN_Tests/directionalTests/Bidirectional.py
+++ Socket_CAN_Tests/directionalTests/Bidirectional.py
@@ -1,16 +1,20 @@
 import time
 import can
+import math
 from cryptography.hazmat.primitives import cmac
 from cryptography.hazmat.primitives.ciphers import algorithms
 from can.message import Message
+#from can.listener import MessageRecipient
 
 import signal
 import asyncio
 from typing import List
 
+
 #Set variables equal to virtual busses running on system
 bustype = 'socketcan'
 channel_0 = 'vcan0'
+channel_1 = 'vcan1'
 channel_2 = 'vcan2'
 
 def handler(signum, frame):
@@ -20,23 +24,41 @@ def handler(signum, frame):
     exit(1)
 signal.signal(signal.SIGINT, handler) 
 
-def speed(mph):
+# convert to speed and include backwards compatibility with can and canfd
+def speed(mph, extended):
+    #1/256 mps
     mps = mph / 2.23694 #convert mph to meter/sec
-    byte2 = math.floor(mps/0.256) #convert m/s to bytes
-    byte1 = math.floor((mps-byte2*0.256)/0.001)
+    
+    # 64 bytes long
+    if extended:
+        # 1/2 ^ 16 calcutates in milliseconds
+        # smallest possible increment that can be represented in a CAN FD frame
+        byte2 = math.floor(mps/0.00390625) #convert m/s to bytes
+        byte1 = math.floor((mps-byte2*0.00390625)/0.00006103516)
+    # 8 bytes long
+    else:
+        # 1/1000
+        #  smallest possible increment that can be represented in a CAN frame
+        byte2 = math.floor(mps/0.256) #convert m/s to bytes
+        byte1 = math.floor((mps-byte2*0.256)/0.001)
+
     return byte1, byte2
 
 #convert hex message to mph
-def pgn(msg)
+def pgn(msg):
     arbitration_ID = hex(msg.arbitration_id).replace("0x", "").upper()
-    pgn_1 = arbitration_ID[0:2]
-    pgn_2 = arbitration_ID[2:4]
+    
+    pgn_1 = int (arbitration_ID[0:2], 16) #convert to int
+    pgn_2 = int (arbitration_ID[2:4], 16) #convert to int
+    
     mps = pgn_2 * 0.256 + pgn_1 * 0.001
     mph = mps * 2.23694
+
     return pgn_1, pgn_2, mph
 
 #Create variables for socketCAN bus channels
 vcan0 = can.interfaces.socketcan.SocketcanBus(channel=channel_0)
+vcan1 = can.interfaces.socketcan.SocketcanBus(channel=channel_1)
 vcan2 = can.interfaces.socketcan.SocketcanBus(channel=channel_2)
 
 def sniffPGN(msg):
@@ -54,19 +76,55 @@ def sniffPGN(msg):
         print(msg)
 
 
-async def main() -> None:
-    reader = can.AsyncBufferedReader()
+async def bridge(bus1, bus2):
+    while True:
+        msg = await bus1.recv()
+        await bus2.send(msg)
 
-    listeners: List[MessageRecipient] = [
-        reader,         # AsyncBufferedReader() listener
-    ]
+async def main() -> None:
+    #reader = can.AsyncBufferedReader()
+    #reader2 = can.AsyncBufferedReader()
+
+    #Async 
+    #listeners: List[MessageRecipient] = [
+    #    reader,         # AsyncBufferedReader() listener
+    #    reader2,        # AsyncBufferedReader() listener for bridge 2
+    #]
 
     loop = asyncio.get_running_loop()
-    notifier = can.Notifier(vcan2, listeners, loop=loop) 
 
-    while True:
-        msg = await reader.get_message()
-        sniffPGN(msg)
+    listener1 = can.Listener()
+    listener2 = can.Listener()
+
+    #notifier1 = can.Notifier(vcan0, listeners, loop=loop) 
+    #notifier2 = can.Notifier(vcan1, listeners, loop=loop)
+    #notifier2 = can.Notifier(vcan2, listeners, loop=loop)
+    
+    # send outgoing messages
+    notifier1 = can.Notifier(vcan0, [listener1, listener2], loop=loop)
+    notifier2 = can.Notifier(vcan1, [listener1, listener2], loop=loop)
+
+    # forward messages between buses
+    bridge1 = asyncio.create_task(bridge(vcan0, vcan2))
+    bridge2 = asyncio.create_task(bridge(vcan1, vcan2))
+
+    # wait until interrupted
+    try:
+        while True:
+            msg = await listener1.get_message()
+            sniffPGN(msg)
+
+            msg2 = await listener2.get_message()
+            sniffPGN(msg2)
+    except KeyboardInterrupt:
+        pass
+
+    #while True:
+    #    msg = await reader.get_message()
+    #    sniffPGN(msg)
+
+    #    msg2 = await reader2.get_message()
+    #    sniffPGN(msg2)
 
 if __name__ == "__main__":
     asyncio.run(main())
