ffeee1294db94e245571ec5e5003bbe23756acc9
codystri <codystri@iastate.edu>
2023-04-03 08:04:10 -0500

    Logging system and slight updates to file

--- Socket_CAN_Tests/ECUDemo/runECUtest.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3 getFramesInf.py & python3 packFrames.py & python3 receiveFrames.py & python3 exampleECU.py
--- Socket_CAN_Tests/run.sh
+++ Socket_CAN_Tests/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 x-terminal-emulator -e "python3 getFrames.py"
-x-terminal-emulator -e "python3 packFrames.py 2> /home/user/Desktop/sdmay23-14/Socket_CAN_Tests/outputData/packFrames.txt"
-x-terminal-emulator -e "python3 receiveFrames.py 2> /home/user/Desktop/sdmay23-14/Socket_CAN_Tests/outputData/receiveFrames.txt"
+x-terminal-emulator -e "python3 packFrames.py"
+x-terminal-emulator -e "python3 receiveFrames.py"
